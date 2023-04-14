0124e5ab53707b9f936f7a227313af95832afec5
hugo <h.almendro.2022@alumnos.urjc.es>
2023-04-14 10:38:56 +0200

    de nada

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+int func(){
+    printf("A ver si bajamos los minimos q no deja tio");
+}
 int main() {
     printf("Hello, World!\nHOLA QUE TAL OSCAR\nHola que tal
            ");
